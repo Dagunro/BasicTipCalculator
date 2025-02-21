768485a32441910fca65c57e8fee87cc6e1c2456
tunjiarofin <tunjiarofin@gmail.com>
2025-02-21 08:06:36 +0100

    Wrote a Local Test

--- app/src/main/java/com/example/basictipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/basictipcalculator/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -59,8 +60,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String{
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String{
     var tip = tipPercent / 100 * amount
     if(roundUp){
         tip = kotlin.math.ceil(tip)
